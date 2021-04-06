a08536b4fe0b074c24b30fefc517c70fe970c20c
TheAssembler1 <lewisnoah592@gmail.com>
2021-04-06 03:33:01 -0500

    Added a background rectangle to refresh windwo

--- Main.py
+++ Main.py
@@ -14,8 +14,8 @@ testObject = RenderableObject(20, 20, 100, 150, fileName)
 Renderer.addRenderableObject(testObject)
 
 running = True
-
 #NOTE::throws an error while exiting but thats ok for now
 while running:
+    testObject.x += 1
     Renderer.render(screen)
     screen.update()
--- Renderer.py
+++ Renderer.py
@@ -7,10 +7,12 @@ class Renderer:
         Renderer.RenderableObjects.append(renderableObject)
 
     def render(window):
+        #NOTE::Background rectangle
+        window.create_rectangle(window.WIDTH, window.HEIGHT, 0, 0, outline="#f11", fill="#1f1", width=0)
+
         for obj in Renderer.RenderableObjects:
             #NOTE::we only need to create image once on canvas so we clear Renderable Objects afterwards
             window.create_image(obj.x, obj.y, anchor=NW, image=obj.bufferedImage)
-        Renderer.RenderableObjects.clear()
             
 
     
